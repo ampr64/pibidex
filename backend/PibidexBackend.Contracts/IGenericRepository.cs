7dc0f08c225ead54a51cc310f89172d16ef4c93f
Álvaro Prieto <alviprieto@hotmail.com>
2020-06-18 22:39:59 -0300

    Add non-implemented IGenericRepository and GenericRepository

---
+++ backend/PibidexBackend.Contracts/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using PibidexBackend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PibidexBackend.Contracts
+{
+    public interface IGenericRepository<TEntity, TPrimaryKey> where TEntity : EntityBase<TPrimaryKey>
+    {
+        public Task<bool> Add(TEntity entity);
+        public Task<TEntity> Find(TPrimaryKey id, bool asTracking, params Expression<Func<TEntity, object>>[] navigationProperties);
+        public Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> filter, params Expression<Func<TEntity, object>>[] navigationProperties);
+        public Task<bool> Remove(TPrimaryKey id);
+        public Task<bool> Remove(TEntity entity);
+        public Task<bool> Update(TEntity entity);
+    }
+}
