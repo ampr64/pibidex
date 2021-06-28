a9c9230ea10cb7c569ada5edcd5942e86ecd85d4
Alvaro Prieto <30221236+ampr64@users.noreply.github.com>
2021-06-28 02:09:41 -0300

    Add domain events interfaces.

---
+++ backend/src/Domain/Common/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Pibidex.Domain.Common
+{
+    public abstract class DomainEvent
+    {
+        public DateTime OccurredOn => DateTime.UtcNow;
+
+        public abstract void Publish();
+    }
+}
---
+++ backend/src/Domain/Common/IHasDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Pibidex.Domain.Common
+{
+    public interface IHasDomainEvent
+    {
+        IReadOnlyList<DomainEvent> DomainEvents { get; }
+
+        void AddDomainEvent(DomainEvent domainEvent);
+
+        void ClearDomainEvents();
+    }
+}
